0c08cdd0cd5e751de9911be35a4e65b50102eff2
victorPC <victorPC@Victor-PC>
2019-11-23 19:25:52 -0200

    base realmente pronta

--- src/paintbrush/D1.java
+++ src/paintbrush/D1.java
@@ -26,7 +26,8 @@ public class D1 extends D0{
         g.drawLine(super.getX(), super.getY(), x1, y1);
     }
     
-    public void comprimento(){
-    
+    public void comprimento(Graphics g){
+        double comprimento = Math.sqrt((x1-super.getX())*(x1-super.getX())+(y1-super.getY())*(y1-super.getY()));
+        g.drawString(""+Math.round(comprimento), (super.getX()+x1)/2,(super.getY()+y1)/2);
     }
 }
